92b92afb941adaf589292a9f1e2c1853e2a0827f
jabertuhin <jabertuhin@gmail.com>
2019-05-02 12:51:40 +0600

    comments are added for more code readability.

--- model_training.py
+++ model_training.py
@@ -7,14 +7,19 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn.externals import joblib
 
+# reading csv file
 df = pd.read_csv("spam.csv", encoding="latin-1")
 
+# dropping unnecesary columns.
 df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
 
+# changing column name
 df.columns = ['label', 'message']
 
+# changing label to 0 and 1
 df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
+# Feature extraction with CountVectorizer
 X = df['message']
 y = df['label']
 cv = CountVectorizer()
@@ -22,6 +27,7 @@ cv_fit = cv.fit(X)
 X = cv.transform(X) # Fit the Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# saving the vector
 pickle.dump(cv_fit, open("vector.pickel", "wb"))
 
 #Naive Bayes Classifier
@@ -31,4 +37,6 @@ clf.score(X_test,y_test)
 y_pred = clf.predict(X_test)
 print(classification_report(y_test, y_pred))
 
+
+# Saving the trained model
 joblib.dump(clf, 'NB_spam_model.pkl')
